2ed5ffa0e80f858d30a1e5cc2a50340300e3dc8d
Sharon Guan <sg1845@att.com>
2013-07-11 13:54:38 -0500

    Third commit

--- web.js
+++ web.js
@@ -4,7 +4,15 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
-  response.send('Hello World 2 !');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
